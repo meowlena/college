4358900f9b7fed2859a0392ee01b87d3277d3303
meowlena <milenabraga98@gmail.com>
2024-01-16 14:55:10 -0300

    setting the library

---
+++ ArqII/perfTests/library/commandLineInput.h
@@ -0,0 +1,22 @@
+#ifndef _COMMANDLINEINPUT_H_
+#define _COMMANDLINEINPUT_H_
+
+#include <stdio.h>
+
+int arguments( int argc, char *argv[] )  {
+    // argv[0] holds the name of the program itself
+    // so, argc>1 hold line arguments
+
+    if( argc == 2 ) {
+      printf("The argument supplied is %s\n", argv[1]);
+    }
+    else if( argc > 2 ) {
+      printf("Too many arguments supplied.\n");
+    }
+    else {
+      printf("One argument expected.\n");
+    }
+}
+
+
+#endif
---
+++ ArqII/perfTests/staticMatrix.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "./library/commandLineInput.h"
+
+int main( int argc, char *argv[] )  {
+    arguments(argc, argv);
+}
